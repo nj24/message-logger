c3728f3a0398947b7882b59db0fe7d798eae2548
Neha Joshi <neha.joshi@kambi.se>
2021-03-21 06:10:58 +0100

    Added Get message endpoint

--- src/main/java/com/qvik/message/logger/MessageLoggerApplication.java
+++ src/main/java/com/qvik/message/logger/MessageLoggerApplication.java
@@ -1,4 +1,4 @@
-package com.qvik.assignment;
+package com.qvik.message.logger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
