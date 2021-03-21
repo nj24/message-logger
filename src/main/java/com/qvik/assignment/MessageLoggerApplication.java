2238a69bb3bd8885d044b07f21fbb22431c03e5c
Neha Joshi <neha.joshi@kambi.se>
2021-03-21 05:41:28 +0100

    Added a message-logger spring boot application

---
+++ src/main/java/com/qvik/assignment/MessageLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.qvik.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qvik/assignment/MessageLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qvik.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
